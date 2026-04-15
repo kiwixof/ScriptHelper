bb3d579d1f30781d49ab1406da8bdae3982d6e4e
kiwixof <kiwixof@gmail.com>
2026-04-15 10:07:27 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,13 +1,5 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))() --iy
 loadstring(game:HttpGet("https://github.com/AZYsGithub/DexPlusPlus/releases/latest/download/out.lua"))() -- dex++
 
--- Hydroxide (remote spy)
-local owner = "Upbolt"
-local branch = "revision"
-
-local function webImport(file)
-    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Hydroxide/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
-end
-
-webImport("init")
-webImport("ui/main")
+-- https://discord.gg/FJcJMuze7S
+loadstring(game:HttpGet("https://github.com/notpoiu/cobalt/releases/latest/download/Cobalt.luau"))() -- Cobalt remote spy
